456b9676e01b840cb0b1f4c9a6f6675c140c4b1f
cchencool <cchencool@gmail.com>
2018-05-07 19:19:53 +0800

    rename project

---
+++ Java/IntelliJProjects/Spring-Boot-Test/src/main/java/hello/WelcomeController.java
@@ -0,0 +1,49 @@
+package hello;
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+    @Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+    @Value("${welcome.name:mm}")
+    private String name = "me2.";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+        model.put("name", this.name);
+		return "welcome";
+	}
+
+
+	@PostMapping("/init")
+    @ResponseBody
+    public String init(@RequestBody Map<String, Object> params) {
+	    String name = "me";
+	    if (null != params) {
+	        name = Optional.of(params.get("name")).orElse("").toString();
+        }
+
+        String resStr = "hello, " + name + "..";
+        Map<String, String> res = new HashMap<String, String>();
+	    res.put("result", resStr);
+
+        String res_json = (new Gson()).toJson(res);
+
+	    return res_json;
+    }
+}
