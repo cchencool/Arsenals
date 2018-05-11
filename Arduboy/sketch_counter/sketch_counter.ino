82f15744b4e6bbc6a5d8dfeadc05916654402a6e
cchencool <cchencool@gmail.com>
2018-05-11 16:20:44 +0800

    update arduboy sketch code format

--- Arduboy/sketch_counter/sketch_counter.ino
+++ Arduboy/sketch_counter/sketch_counter.ino
@@ -1,9 +1,10 @@
 
 #include "Arduboy.h"
+#include <avr/pgmspace.h>
 
 Arduboy arduboy;
 
-String hello_str = "hello, my friend.";
+PROGMEM const String hello_str = "hello, my friend.";
 
 int count = 0;
 int count_times = 1;
@@ -14,7 +15,8 @@ int lock_free_frame_count = 0;
 boolean lock = false;
 boolean lock_free = false;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
   arduboy.begin();
@@ -22,22 +24,17 @@ void setup() {
   arduboy.setFrameRate(15);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if (!(arduboy.nextFrame())) {
+  if (!(arduboy.nextFrame()))
+  {
     return;
   }
 
   arduboy.clear();
 
-  arduboy.drawLine(0, 0, 5, 5, WHITE);
-  arduboy.drawLine(5, 5, 122, 5, WHITE);
-  arduboy.drawLine(5, 5, 5, 58, WHITE);
-  arduboy.drawLine(122, 5, 127, 0, WHITE);
-  arduboy.drawLine(122, 5, 122, 58, WHITE);
-  arduboy.drawLine(122, 58, 127, 63, WHITE);
-  arduboy.drawLine(5, 58, 122, 58, WHITE);
-  arduboy.drawLine(5, 58, 0, 63, WHITE);
+  draw_square();
 
   do_count();
 
@@ -57,75 +54,85 @@ void loop() {
   arduboy.display();
 }
 
-void check_lock_free_flag() {
-
-  if ( arduboy.pressed(A_BUTTON) == true
-       || arduboy.pressed(B_BUTTON) == true
-       || arduboy.pressed(UP_BUTTON) == true
-       || arduboy.pressed(DOWN_BUTTON) == true
-       || arduboy.pressed(RIGHT_BUTTON) == true
-       || arduboy.pressed(LEFT_BUTTON) == true) {
-    lock_free_frame_count ++;
-  }
-
-  if (lock_free_frame_count >= lock_free_frame_limit) {
-    lock = false;
-    lock_free = true;
-  }
-
-}
-
-void do_count() {
+void do_count()
+{
 
   // up
-  if ( arduboy.pressed(UP_BUTTON) == true && (!lock || lock_free)) {
-    count += count_times ;
+  if (arduboy.pressed(UP_BUTTON) == true && (!lock || lock_free))
+  {
+    count += count_times;
     lock = true;
-    lock_free_frame_count ++;
+    lock_free_frame_count++;
   }
 
   // down
-  if ( arduboy.pressed(DOWN_BUTTON) == true && (!lock || lock_free)) {
-    count -= count_times ;
+  if (arduboy.pressed(DOWN_BUTTON) == true && (!lock || lock_free))
+  {
+    count -= count_times;
     lock = true;
   }
 
   // right
-  if ( arduboy.pressed(RIGHT_BUTTON) == true && (!lock || lock_free)) {
-    count_times *= 10 ;
+  if (arduboy.pressed(RIGHT_BUTTON) == true && (!lock || lock_free))
+  {
+    count_times *= 10;
     lock = true;
   }
 
   // left
-  if ( arduboy.pressed(LEFT_BUTTON) == true && (!lock || lock_free)) {
-    count_times *= 0.1 ;
-    if (count_times == 0) {
+  if (arduboy.pressed(LEFT_BUTTON) == true && (!lock || lock_free))
+  {
+    count_times *= 0.1;
+    if (count_times == 0)
+    {
       count_times = 1;
     }
     lock = true;
   }
 
   // func
-  if ( (arduboy.pressed(A_BUTTON) == true || arduboy.pressed(B_BUTTON) == true) && (!lock || lock_free)) {
+  if ((arduboy.pressed(A_BUTTON) == true || arduboy.pressed(B_BUTTON) == true) && (!lock || lock_free))
+  {
     count = 0;
     count_times = 1;
     lock = true;
   }
-
 }
 
-void check_lock_flag() {
+void draw_square()
+{
+  arduboy.drawLine(0, 0, 5, 5, WHITE);
+  arduboy.drawLine(5, 5, 122, 5, WHITE);
+  arduboy.drawLine(5, 5, 5, 58, WHITE);
+  arduboy.drawLine(122, 5, 127, 0, WHITE);
+  arduboy.drawLine(122, 5, 122, 58, WHITE);
+  arduboy.drawLine(122, 58, 127, 63, WHITE);
+  arduboy.drawLine(5, 58, 122, 58, WHITE);
+  arduboy.drawLine(5, 58, 0, 63, WHITE);
+}
 
-  if ( arduboy.notPressed(A_BUTTON) == true
-       && arduboy.notPressed(B_BUTTON) == true
-       && arduboy.notPressed(UP_BUTTON) == true
-       && arduboy.notPressed(DOWN_BUTTON) == true
-       && arduboy.notPressed(RIGHT_BUTTON) == true
-       && arduboy.notPressed(LEFT_BUTTON) == true) {
+void check_lock_free_flag()
+{
+
+  if (arduboy.pressed(A_BUTTON) == true || arduboy.pressed(B_BUTTON) == true || arduboy.pressed(UP_BUTTON) == true || arduboy.pressed(DOWN_BUTTON) == true || arduboy.pressed(RIGHT_BUTTON) == true || arduboy.pressed(LEFT_BUTTON) == true)
+  {
+    lock_free_frame_count++;
+  }
+
+  if (lock_free_frame_count >= lock_free_frame_limit)
+  {
+    lock = false;
+    lock_free = true;
+  }
+}
+
+void check_lock_flag()
+{
+
+  if (arduboy.notPressed(A_BUTTON) == true && arduboy.notPressed(B_BUTTON) == true && arduboy.notPressed(UP_BUTTON) == true && arduboy.notPressed(DOWN_BUTTON) == true && arduboy.notPressed(RIGHT_BUTTON) == true && arduboy.notPressed(LEFT_BUTTON) == true)
+  {
     lock = false;
     lock_free = false;
     lock_free_frame_count = 0;
   }
 }
-
-
