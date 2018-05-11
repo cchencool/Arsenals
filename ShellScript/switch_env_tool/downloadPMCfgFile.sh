6daa2613266d1e99643462d96b825ddd79fb782d
cchencool <cchencool@gmail.com>
2018-05-11 13:14:42 +0800

    update to support C30

--- ShellScript/switch_env_tool/downloadPMCfgFile.sh
+++ ShellScript/switch_env_tool/downloadPMCfgFile.sh
@@ -4,6 +4,7 @@ basepath=$(cd `dirname $0`; pwd)
 server=$1
 
 let finalResult=0
+is_before_v2=false
 
 function initParam()
 {
@@ -12,23 +13,39 @@ function initParam()
 	usr=acrosspm
 	
 	local_cmn=/opt/netwatcher/pm4h2/work/conf/
-	local_jsc=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/jre/lib/security/
-	local_wrk=/encrypt
-	local_rsc=/encrypt/rootsecu
-	local_erp=/encrypt/encrypt
-	local_drp=/encrypt/decrypt
+
+	if [ ${is_before_v2} == true ];then
+		local_jsc=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/jre/lib/security/
+		local_rsc=/encrypt/rootkey1/
+		local_erp=/encrypt/rootkey2/
+		local_drp=/encrypt/rootkey3/
+	else
+		local_jsc=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/jre/lib/security/
+		local_rsc=/encrypt/rootsecu/
+		local_erp=/encrypt/encrypt/
+		local_drp=/encrypt/decrypt/
+	fi
+	local_wrk=/encrypt/
 	local_krb_cnf=/opt/netwatcher/pm4h2/app/opt/krb5-1.15.2/conf/indata/krb5.conf
-	local_krb_key=/netwatcher/pm4h2/app/opt/krb5-1.15.2/var/krb5kdc/hbase.keytab
+	local_krb_key=/opt/netwatcher/pm4h2/app/opt/krb5-1.15.2/var/krb5kdc/hive.service.keytab
 	local_dsk=/Users/Chen/Desktop
 	
 	server_cmn=/opt/netwatcher/pm4h2/app/conf/common.properties
-	server_jsc=/opt/netwatcher/pm4h2/app/opt/jdk1.8.0_${jdk_ver}/jre/lib/security/jssecacerts
-	server_wrk=/opt/netwatcher/pm4h2/encrypt/work_key.properties
-	server_rsc=/opt/netwatcher/pm4h2/work/conf/security/rootsecu/rootsecu.txt
-	server_erp=/opt/netwatcher/pm4h2/work/conf/encrypt/encrypt.txt
-	server_drp=/opt/netwatcher/pm4h2/work/conf/decrypt/decrypt.txt
+	#if [ ${server} -eq 124 ];then
+	if [ ${is_before_v2} == true ];then
+		server_jsc=/opt/netwatcher/pm4h2/app/opt/jdk1.7.0_${jdk_ver}/jre/lib/security/jssecacerts
+		server_rsc=/opt/netwatcher/pm4h2/work/conf/security/rootkey1/rootkey1.txt
+		server_erp=/opt/netwatcher/pm4h2/work/conf/security/rootkey2/rootkey2.txt
+		server_drp=/opt/netwatcher/pm4h2/work/conf/security/rootkey3/rootkey3.txt
+	else
+		server_jsc=/opt/netwatcher/pm4h2/app/opt/jdk1.8.0_${jdk_ver}/jre/lib/security/jssecacerts
+		server_rsc=/opt/netwatcher/pm4h2/work/conf/security/rootsecu/rootsecu.txt
+		server_erp=/opt/netwatcher/pm4h2/work/conf/encrypt/encrypt.txt
+		server_drp=/opt/netwatcher/pm4h2/work/conf/decrypt/decrypt.txt		
+	fi
 	server_krb_cnf=/opt/netwatcher/pm4h2/app/opt/krb5-1.15.2/conf/indata/krb5.conf
-	server_krb_key=/netwatcher/pm4h2/app/opt/krb5-1.15.2/var/krb5kdc/hbase.keytab
+	server_krb_key=/opt/netwatcher/pm4h2/app/opt/krb5-1.15.2/var/krb5kdc/hive.service.keytab
+	server_wrk=/opt/netwatcher/pm4h2/encrypt/work_key.properties
 }
 
 function doScp()
@@ -63,6 +80,9 @@ function doScp()
 function begin()
 {
 	jdk_ver=$1
+	if [ -n "$3" ]; then
+		is_before_v2=true
+	fi
 	initParam
 	doScp $2
 }
@@ -86,6 +106,9 @@ function chAuth()
 	chmod 664 ${local_rsc}rootsecu.txt 
 	chmod 664 ${local_erp}encrypt.txt
 	chmod 664 ${local_drp}decrypt.txt
+	chmod 664 ${local_rsc}rootkey1.txt 
+	chmod 664 ${local_erp}rootkey2.txt
+	chmod 664 ${local_drp}rootkey3.txt
 }
 
 if [ ${server} -eq 56 ];then
@@ -93,7 +116,7 @@ if [ ${server} -eq 56 ];then
 elif [ ${server} -eq 57 ];then
 	begin 101 Across_pwd_001
 elif [ ${server} -eq 124 ];then
-	begin 151 Across_pwd_001
+	begin 79 "Acrosspm@123" true
 elif [ ${server} -eq 158 ];then
 	begin 121 Across_pwd_001
 elif [ ${server} -eq 156 ];then
